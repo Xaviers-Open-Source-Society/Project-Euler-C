0afd88a585cd3794a69d5eebd1a2d6e10c326a72
Sonagra Sumit <sonagrasumit@gmail.com>
2017-09-18 19:51:12 +0530

    Update E1S.c

--- E1S.c
+++ E1S.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int sum_multiples(int );
+int main(){
+	int number = 1000;
+	printf("Sum is %d.\n", sum_multiples(number));
+}
+int sum_multiples(int number2){
+	int i, sum = 0;
+	for(i = 0; i < number2; i++){
+		if(i % 3 == 0 || i % 5 == 0){
+			sum = sum + i;
+		}
+		else
+			continue;
+	}
+	return(sum);
+}
 
