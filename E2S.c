0d7560ce6d8797d142e62a00526029b3b92d7eda
Sonagra Sumit <sonagrasumit@gmail.com>
2017-09-18 20:54:19 +0530

    Update E2S.c

--- E2S.c
+++ E2S.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int fib(int , int);
+int main(){
+	int fib1 = 1, fib2 = 2, sum = 0, fibn = 0;
+	while(fibn <= 4000000){
+		fibn = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fibn;
+		if(fibn % 2 == 0)
+			sum = sum + fibn;
+	}
+	printf("Sum of even-valued terms smaller than four million is %d.\n", sum + 2);
+}
