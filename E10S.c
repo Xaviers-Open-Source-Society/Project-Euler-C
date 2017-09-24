6c9a35e3124cc5b27e7a13798136b240b3b5fe71
shivam-199 <32065724+shivam-199@users.noreply.github.com>
2017-09-24 11:41:56 -0700

    Update E10S.c

--- E10S.c
+++ E10S.c
@@ -1,1 +1,29 @@
-
+/*The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ Find the sum of all the primes below two million.
+ */
+#include<stdio.h>
+#include<math.h>
+ int prime_check(long  number)
+{
+	int i;
+	 for (i=2;i<=sqrt(number);i++)
+       {
+       	 if (number%i==0)
+       	  {
+			 return 0;;
+		  }
+	   }
+	   
+	 return 1;
+}
+void main()
+{
+     long  sum=0;
+     int i;
+	 for (i=2;i<2000000;i++)
+	 {
+	    if (prime_check(i))
+		 sum = sum + i;	
+	 }
+     printf("\n\n Required sum : %ld",sum);
+}
