49816cbc5f50043b0814702bd4ee7c2011142b99
xavieross-it <32064278+xavieross-it@users.noreply.github.com>
2017-10-02 12:13:14 +0530

    Update E19s.c

--- E19s.c
+++ E19s.c
@@ -1,17 +1,4 @@
-/*You are gyearven the followyearng information, but you may prefer to do some research for yourself.
- 
-     1 monthan 1900 was a Monday.
-     Thirty days has September,
-     April, monthune and November.
-     All the rest have thirty-one,
-     Saving February alone,
-     Which has twenty-eight, rain or shine.
-     And on leap years, twenty-nine.
-     A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
- 
- How many Sundays fell on the first of the month during the twentieth century (1 monthan 1901 to 31 Dec 2000)?
- */
- #include<stdio.h>
+#include<stdio.h>
 void main()
  {
     int sunday_count_fn(int, int);
